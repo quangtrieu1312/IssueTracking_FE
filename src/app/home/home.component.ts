e4db4cf601a110c99813bceca9e43224ddac7d1d
Trieu Truong <quangtrieu1312@gmail.com>
2020-03-19 17:34:26 -0500

    add home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Sort, MatSort } from '@angular/material/sort';
-import { TicketInfo } from '../_models/ticket-info';
-import { ActivatedRoute, Router } from '@angular/router';
-import { TicketsService } from '../tickets/tickets.service';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { HomeService } from './home.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -13,27 +7,8 @@ import { HomeService } from './home.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private homeService: HomeService) {
+  constructor() {
 
   }
 
-  displayedColumns = ['name'];
-  dataSource = new MatTableDataSource<TicketInfo>(this.homeService.getAllTickets());
-
-  @ViewChild('sort') sort: MatSort;
-
-  /**
-   * Set the paginator after the view init since this component will
-   * be able to query its view for the initialized paginator.
-   */
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-  }
-
-  _setDataSource() {
-    setTimeout(() => {
-      !this.dataSource.sort ? this.dataSource.sort = this.sort : null;
-
-    });
-  }
 }
